21ebe3cc7224ff7f6553699429a4b1abaecdae58
Julio Fruta <julio.guzman@me.com>
2024-02-20 20:36:44 -0800

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Julio Cesar Guzman Villanueva on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
