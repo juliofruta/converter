a8c423ae53bfbdb9af096b3f202597abd625ceab
Julio Fruta <julio.guzman@me.com>
2024-02-21 18:56:09 -0800

    converter

--- converter/ContentView.swift
+++ converter/ContentView.swift
@@ -6,17 +6,45 @@
 //
 
 import SwiftUI
+import spatialvideo
 
 struct ContentView: View {
+    @State private var image = Image(systemName: "bolt")
+    @State private var description = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .dropDestination(for: Data.self) { sideBySideVideos, location in
+                    image = Image(systemName: "bolt.fill")
+                    Task {
+                        for sideBySideVideo in sideBySideVideos {
+                            do {
+                                let videoPath = try await spatialVideo(with: sideBySideVideo)
+                                print("MV-HEVC video encoded to \(videoPath)")
+                                image = Image(systemName: "bolt")
+                            } catch {
+                                print("error \(error)")
+                                description = error.localizedDescription
+                                image = Image(systemName: "bolt.trianglebadge.exclamationmark")
+                            }
+                        }
+                    }
+                    return true
+                }
+            Text(description)
         }
-        .padding()
     }
+}
+
+func spatialVideo(with videoData: Data) async throws -> URL {
+    let sideBySideVideoURL = URL.documentsDirectory.appending(path: "input-\(UUID().uuidString).mp4")
+    try videoData.write(to: sideBySideVideoURL, options: [.atomic, .completeFileProtection])
+    let spatialVideo = try await spatialVideo(from: sideBySideVideoURL)
+    return spatialVideo
 }
 
 #Preview {
--- converterTests/converterTests.swift
+++ converterTests/converterTests.swift
@@ -9,28 +9,11 @@ import XCTest
 @testable import converter
 
 final class converterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSpatialVideo() async throws {
+        let path = Bundle.init(for: type(of: self)).path(forResource: "input", ofType: "mp4")!
+        let inputURL = URL.init(filePath: path)
+        let data = try Data(contentsOf: inputURL)
+        let url = try await converter.spatialVideo(with: data)
+        print(url)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
